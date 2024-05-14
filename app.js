d3f12adc32cefa40e7ea4e776458695b72938622
Luís Felipe Vidotti <luisfelipealmeida66@gmail.com>
2024-05-13 23:45:26 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require("dotenv").config();
+const DB_SENHA = process.env.DB_SENHA;
+const DB_USER = process.env.DB_USER;
+const PORT = 3000;
+
+//config
+    //body-parser
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    //mongoose
+    mongoose.connect(`mongodb+srv://${DB_USER}:${DB_SENHA}@cluster0.ayipw1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+        console.log("Conectado ao banco de dados com sucesso!!!");
+    }).catch((erro) => {
+        console.log("Erro ao se conectar ao banco de dados");
+    })
+
+//rotas
+app.get("/", (req,res) => {
+    res.send("Ola mundo");
+})
+
+//server
+app.listen(PORT, () => {
+    console.log(`Servidor rodando da porta ${PORT}`);
+})
